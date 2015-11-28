fc22afb5e78937b88dd121fc3125b098b5eea895
Jay SyKo <jay@jaysyko.com>
2015-11-27 21:09:27 -0500

    Added Basic Timer Functionality

--- mobile/src/main/java/com/jaysyko/sykodoro/MainActivity.java
+++ mobile/src/main/java/com/jaysyko/sykodoro/MainActivity.java
@@ -1,32 +1,67 @@
 package com.jaysyko.sykodoro;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button timeControlButton;
+    private TextView timerValue;
+    private long initValue = 0L;
+    private Handler handler = new Handler();
+    long timeMilli = 0L;
+    long timeSwapBuff = 0L;
+    long updatedTime = 0L;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        timerValue = (TextView) findViewById(R.id.timerValue);
+        timeControlButton = (Button)findViewById(R.id.controlButton);
+        timeControlButton.setOnClickListener(new View.OnClickListener() {
 
-       FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                if (timeControlButton.getText() == "Start") {
+                    timeControlButton.setText("Pause");
+                    initValue = SystemClock.uptimeMillis();
+                    handler.postDelayed(updateTimerThread, 0);
+                } else {
+                    timeControlButton.setText("Start");
+                    timeSwapBuff += timeMilli;
+                    handler.removeCallbacks(updateTimerThread);
+                }
             }
         });
+
+        setSupportActionBar(toolbar);
     }
+
+    private Runnable updateTimerThread = new Runnable() {
+        @Override
+        public void run() {
+            timeMilli = SystemClock.uptimeMillis() - initValue;
+            updatedTime = timeSwapBuff + timeMilli;
+            int secs = (int) (updatedTime / 1000);
+            int mins = secs/60;
+            secs %= 60;
+            int milli = (int)(updatedTime %1000);
+            final String seconds = String.format("%02d", secs);
+            final String milliseconds = String.format("%03d", milli);
+            final String time = String.format("%s:%s:%s", mins, seconds, milliseconds);
+            timerValue.setText(time);
+            handler.postDelayed(this, 0);
+        }
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- mobile/src/main/java/com/jaysyko/sykodoro/MainActivityFragment.java
+++ mobile/src/main/java/com/jaysyko/sykodoro/MainActivityFragment.java
@@ -16,7 +16,7 @@ public class MainActivityFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
+                             Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
